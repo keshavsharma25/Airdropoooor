116e610ecf0aeaae0de819df91e91f3d0696cad0
keshavsharma25 <skeshav0825@gmail.com>
2024-04-20 15:47:18 +0530

    feat: add deploy script for Registry & Factory

--- packages/hardhat/deploy/01_deploy_AirdropoooorFactory.ts
+++ packages/hardhat/deploy/01_deploy_AirdropoooorFactory.ts
@@ -8,7 +8,7 @@ import { Contract } from "ethers";
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployAirdropoooorFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,7 +22,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("AirdropoooorFactory", {
     from: deployer,
     // Contract constructor arguments
     args: [deployer],
@@ -33,12 +33,12 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const AirdropoooorFacotry = await hre.ethers.getContract<Contract>("AirdropoooorFacotry", deployer);
+  console.log("AirdropoooorFactory Contract Address is: ", AirdropoooorFacotry.getAddress());
 };
 
-export default deployYourContract;
+export default deployAirdropoooorFactory;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployAirdropoooorFactory.tags = ["AirdropoooorFactory"];
